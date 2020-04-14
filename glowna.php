63be0f24c103c7e2aedd022810bf949f64a0202f
Deggeri <lukaszdziadosz550@gmail.com>
2020-04-14 16:23:50 +0200

    Dodanie ikonek mediów społecznościowych do headera

--- glowna.php
+++ glowna.php
@@ -15,7 +15,15 @@ session_start();
 			<div id="logo">
 				<h1><a href="index.php">Szybkie auta</a></h1>
 				<p>Design by Łukasz Dziadosz</a></p>
-      </div>
+	  </div>
+	  <div id="social">
+				<ul class="contact" style="list-style:none;">
+					<li style="float:left"><a href="#" class="icon icon-twitter"><img src="img/twitter.png" style="width:100px;"></a></li>
+					<li style="float:left"><a href="#" class="icon icon-facebook"><img src="img/facebook.png" style="width:100px;"></a></li>
+					<li style="float:left"><a href="#" class="icon icon-dribbble"><img src="img/Google.png" style="width:100px;"></a></li>
+					<li style="float:left"><a href="#" class="icon icon-tumblr"><img src="img/Ig.png" style="width:100px;"></a></li>
+				</ul>
+			</div>
     </div>
 		<div id="menu" class="container">
         <form action="includes/logout.inc.php" method="post">
