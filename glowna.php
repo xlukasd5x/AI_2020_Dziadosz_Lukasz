c5066e88184c5eada21d4550a417e1f7e09c43c6
Deggeri <lukaszdziadosz550@gmail.com>
2020-05-20 00:00:15 +0200

    Zrobienie całej strony głównej

--- glowna.php
+++ glowna.php
@@ -40,6 +40,47 @@ session_start();
 		</div>
 	</div>
 </div>
-<form action="includes/logout.inc.php" method="post">
-		 <button type="submit" name="logout-submit">Logout</button>
-	  </form>
+<div>
+<h3 style="text-align:center;font-size:26px">Nalepsza wypożyczalnia w tym regionie!</h3>
+	<p style="text-align:justify;padding-left:300px;padding-right:300px">Jak najlepiej zwiedzać Podkarpacie? Otóż mając samochód pod ręką! Dzięki pojazdowi na Twoje usługi będziesz mógł podjechać, gdzie tylko zechcesz. Wynajęty samochód znacznie upraszcza życie i fantastycznie umila wakacyjny wypoczynek. Wypożyczalnia samochodów <strong>Szybkie auta - Łukasz Dziadosz</strong> otworzyła punkt obsługi w Rzeszowie, aby umożliwić Ci szybkie i bezpieczne podróżowanie po województwie podkarpackim.
+	<br>
+	Auto możesz wypożyczyć od nas na krócej lub dłużej. Fakt  faktem im dłużej tym korzystniej! Wynajętym autem możesz wyjechać nawet za granicę Polski.
+	<br>
+	Zapraszamy do działu <strong><a href="#" style="text-decoration: none">rezerwacja</a></strong>.</p>
+	<h4 style="text-align:center;font-size:26px">Jakim autem wyjechać z Sanoka?</h4>
+	<p style="text-align:justify;padding-left:300px;padding-right:300px">Wybór samochodu do wynajęcia jest bardzo subiektywną czynnością i my to świetnie rozumiemy. Z tego względu zadbaliśmy, aby oferta naszej wypożyczalni samochodów była jak najbardziej rozbudowana. Flota <strong>Szybkie auta - Łukasz Dziadosz</strong> prezentuje się następująco:</p>
+    <ul style="text-align:justify;padding-left:318px;padding-right:300px">
+    <li><strong>auta osobowe</strong> – możesz wypożyczyć popularne marki, np. Hyundai i20, Seat Leon</li>
+	<li><strong>samochody dostawcze</strong> – załaduj się do Peugeot Boxer lub Fiat Ducato</li>
+	<li><strong>pojazdy luksusowe</strong> – co powiesz na Peugeot 508?</li>
+	<li><strong>busy</strong> – dla Ciebie i Twojej rodziny wynajmij busa Fiat Scudo lub Renault Trafic</li>
+	</ul>
+	<p style="text-align:justify;padding-left:300px;padding-right:300px">Zapraszamy do <strong><a href="#" style="text-decoration: none">cennika</a></strong></p>
+	<img src="img/Auto1.jpg" alt="auto1" style="width:100%">
+	<h5 style="text-align:center;font-size:26px">Rzeszów - stolica Podkarpacia</h5>
+	<p style="text-align:justify;padding-left:300px;padding-right:300px"><strong>Rzeszów</strong>, miasto będące stolicą utworzonego w 1999 roku województwa podkarpackiego, leżące w południowo – wschodniej Polsce. Stanowi miasto na prawach powiatu, a według najnowszych danych statystycznych, liczy około 180 tysięcy mieszkańców. <strong>Rzeszów</strong> znajduje się na granicy <strong>Pogórza Karpackiego i Kotliny Sandomierskiej</strong>, nad liczącą 220 kilometrów długości <strong>rzeką Wisłok</strong>.</p>
+    <p style="text-align:justify;padding-left:300px;padding-right:300px">Historię osad na terenie dzisiejszego <strong>Rzeszowa</strong> potwierdzają wykopaliska archeologiczne dokumentujące odkrycia sprzed 6 tysięcy lat. Między XI a XII wiekiem, z inicjatywy prawdopodobnie Polaków, powstał gród miejski. Prawa miejskie z kolei nadane zostały 19 stycznia 1354 roku, kiedy to Kazimierz III Wielki przekazał miasto Janowi Pakosławicowi ze Stróżysk. Mimo dewastacji w XV wieku, <strong>Rzeszów</strong> został szybko odbudowany, a na mocy uzyskanych przywilejów zdobył możliwość szybkiego rozwoju. Już wtedy w mieście rozwinęło się m.in. kowalstwo, krawiectwo czy kuśnierstwo. Po bitwie pod Rzeszowem, 13 sierpnia 1769 roku, miasto przez 146 lat wchodziło w skład Imperium Habsburgów. Na powrót wolnym stało się w roku 1845. W czasie II wojny światowej, w Rzeszowie istniało getto żydowskie, które zamieszkiwało około 30% ówczesnych mieszkańców miasta. Większość ze znajdujących się w nim Żydów wywieziono do obozu lub rozstrzelano.</p>
+	<p style="text-align:justify;padding-left:300px;padding-right:300px">Obecnie <strong>Rzeszów</strong> stopniowo powiększa swój obszar i dynamicznie się rozwija.</p>
+    <h6 style="text-align:center;font-size:26px">Rzeszów – zwiedzaj miasto </h6>
+	<p style="text-align:justify;padding-left:300px;padding-right:300px">Do najważniejszych atrakcji i zabytków <strong>Rzeszowa</strong> z pewnością należy zaliczyć:</p>
+	<ul style="text-align:justify;padding-left:318px;padding-right:300px">
+	<li>Ratusz Miejski znajdujący się na Starym Mieście. Wybudowany został najprawdopodobniej w 1591 roku, jednak obecną neogotycką i neorenesansową formę zyskał w II połowie XIX wieku. W Ratuszu wciąż znajduje się siedziba Urzędu i Prezydenta Miasta.</li>
+	<li>Wybudowany w latach 1902 – 1906 Zamek, powstały na miejscu zbudowanego ok. XVI wieku zamku Lubomirskich. Pierwotnie planowano jedynie renowację istniejącego zamku, jednak ze względu na jego zły stan podjęto decyzję o całkowitym wyburzeniu.</li>
+	<li>Aleja Pod Kasztanami stanowiąca malowniczy deptak z zabytkowymi zabudowami.</li>
+	<li>Letni Pałac Lubomirskich, powstały w latach 90. XVII wieku.</li>
+	<li>Teatr im. Wandy Siemaszkowej, zbudowany w latach 1890 – 1900 jest obecnie jednym z najważniejszych zabytków Rzeszowa i ważnym elementem kulturalnym regionu. Rokrocznie organizuje festiwale: Spotkania Teatralne, Karnawałowe oraz Plenerowe.</li>
+	<li>Założony w 1849 roku Cmentarz żydowski na Czekaju, zdewastowany podczas II wojny światowej. Do czasów współczesnych zachowały się 754 nagrobki.</li>
+	<li>Cmentarz Pobitno, otwarty 1 stycznia 1910 roku, liczący około 7 ha powierzchni.</li>
+	<li>Stanowiący jeden z najstarszych Stary Cmentarz, zamknięty w momencie otwarcia Cmentarza Pobitno. Został założony w 1792 roku, według szacunków, na jego terenie spoczywa około 40 tys. osób.</li>
+	<li>Wielokrotnie przebudowywana wskutek pożarów Synagoga Staromiejska, pierwotnie powstała w roku 1610.</li>
+	</ul>
+	<img src="img/Auto2.jpg" alt="auto2" style="width:100%">
+	<h6 style="text-align:center;font-size:26px">Rzeszów – smiejscowości w sąsiedztwie</h6>
+	<p style="text-align:justify;padding-left:300px;padding-right:300px">Wypożyczalnia <strong>Szybkie auta - Łukasz Dziadosz</strong> proponuje wynajem auta z własnej floty, jako dobrą opcję dla komfortowej podróży. Rzeszów i jego okolice pełne są małych i dużych miejscowości z atrakcjami dla każdego turysty. Polecamy interesujące miasta i miasteczka</p>
+	<ul style="text-align:justify;padding-left:318px;padding-right:300px">
+	<li><strong>Sanok: </strong>jest jednym z najdalej wysuniętych na południowy wschód ośrodków miejskich Polski, z rozwiniętym przemysłem chemicznym.</li>
+	<li><strong>Solina: </strong>słynie z Jeziora Solińskiego, które powstało w 1968 r. Atrakcją jest możliwość zwiedzania zabory spacerując po koronie, jak również w jej betonowych podziemiach.</li>
+	<li><strong>Brzozów: </strong>położony jest w dawnej ziemi sanockiej. Miasto powstało w połowie XIV wieku. Nazwa pochodzi od lasu Brzozowe. Pod koniec XIV wieku znalazło się w posiadaniu biskupów przemyskich i do końca XVIII wieku było rezydencją jego właścicieli.</li>
+	<li><strong>Leżajsk: </strong>miasto w Polsce, w północnej części województwa podkarpackiego, siedziba powiatu leżajskiego. Leży w dolinie Sanu, na skraju dawnej Puszczy Sandomierskiej, w dawnej ziemi przemyskiej.</li>
+	</ul>
+</div>
