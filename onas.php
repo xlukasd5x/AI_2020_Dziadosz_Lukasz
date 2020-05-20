2e0b6400eb55eaee282dc435b1f84fe3494bbc23
Deggeri <lukaszdziadosz550@gmail.com>
2020-05-20 23:29:42 +0200

    Cała podstrona "o nas"

--- onas.php
+++ onas.php
@@ -40,5 +40,34 @@ session_start();
 		</div>
 	</div>
 	<div>
-<iframe class="mapa" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2562.9885259316393!2d22.013391215884763!3d50.03031082542907!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473cfaf83c5a9833%3A0x432f7dd9b86f7a01!2sUniwersytet%20Rzeszowski!5e0!3m2!1spl!2spl!4v1589833467475!5m2!1spl!2spl" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0" ></iframe>
+    <h3 style="text-align:center;font-size:26px;margin:0;padding-top:26px;background:#F8F8F8" >Ogólnopolska wypożyczalnia samochodów Szybkie auta - Łukasz Dziadosz</h3>
+	<section class="onas-left" style="float:left;width:47%;">
+	<h4 style="padding-left:310px;font-size:20px">Nowi na polskim rynku</h4>
+    <p style="text-align:justify;padding-left:310px;">Firma samochodów <strong>Szybkie auta - Łukasz Dziadosz</strong> buduje w Polsce największą sieć wypożyczalni samochodów. Obecnie wynająć auto można w 31 miastach, głównie w Rzeszowie ale także m.in.: Bydgoszcz, Częstochowa, Gdańsk, Gdynia, Kalisz, Katowice, Kielce, Koszalin, Kraków, Lubin, Olsztyn, Poznań, Szczecin, Toruń, Warszawa, Wrocław.</p>
+	<p>&nbsp;</p>
+	<p>&nbsp;</p>
+	<img src="img/Auto4.jpg" alt="auto4" style="width:100%;padding-left:310px">
+	<p>&nbsp;</p>
+	<p>&nbsp;</p>
+	<h4 style="padding-left:310px;font-size:20px">Profesjonalna obsługa</h4>
+	<p style="text-align:justify;padding-left:310px">Dla pracowników firmy <strong>Szybkie auta - Łukasz Dziadosz</strong> najważniejszy jest Klient, dlatego też stale wychodzimy naprzeciw oczekiwaniom naszych Klientów. Wszystko to z myślą o owocnej i długoletniej współpracy. Mamy na uwadze aktualne trendy i potrzeby. Stajemy na wysokości zadania i oferujemy najnowsze modele samochodów dające użytkownikom gwarancję bezpieczeństwa i komfortu jazdy.</p>
+	<p style="text-align:justify;padding-left:310px">Aby zapewnić jak najlepszą obsługę, zatrudniamy ponad 100 specjalistów, którzy zapewniają naszym Klientom profesjonalizm i wyjście nawet z nieoczekiwanych sytuacji. Ogólnopolska infolinia czynna 7 dni w tygodniu umożliwia Klientom stały kontakt z pracownikami firmy. Jesteśmy elastyczni zarówno pod względem zróżnicowanej floty jak i warunków wynajmów. </p>
+	</section>
+	<section class="onas-right" style="float:right;width:47%">
+	<h4 style="font-size:20px">Najwyższa jakość</h4>
+	<p style="text-align:justify;padding-right:310px">Wypożyczalnia samochodów <strong>Szybkie auta - Łukasz Dziadosz</strong> cechuje się wysoką jakością oferowanych samochodów. Wszystkie nasze pojazdy są nowe: najstarszy model ma 2 lata. Ze szczególną starannością dbamy o sprawność techniczną pojazdów, dlatego stale przeprowadzamy przeglądy techniczne w autoryzowanych serwisach.</p>
+	<p>&nbsp;</p>
+	<p>&nbsp;</p>
+	<h4 style="font-size:20px">Dlaczego my?</h4>
+	<ul style="line-height:180%">
+	<li>Posiadamy nowe samochody</li>
+	<li>Zapewniamy atrakcyjne ceny</li>
+	<li>Nasze samochody są bezpieczne</li>
+	<li>Jesteśmy dostępni w całej Polsce</li>
+	</ul>
+	<p style="text-align:justify;padding-right:310px"><strong>Szybkie auta - Łukasz Dziadosz</strong> w swojej ofercie posiada ponad 2500 nowych samochodów osobowych oraz ponad 500 samochodów dostawczych. Flota samochodów jest serwisowana u autoryzowanych dealerów. Posiadamy marki premium takie jak: BMW, Mercedes, Audi, Lexus i Volvo, a także bardzo duży wybór samochodów osobowych takich jak Mazda, Seat, Ford, Peugeot, Suzuki, Toyota, Volkswagen, Kia, Renault, Nissan.</p>
+	<p>&nbsp;</p>
+	<p>&nbsp;</p>
+	<img src="img/Auto3.jpg" alt="auto4" style="width:100%;padding-right:310px">
+	</section>
 	</div>
